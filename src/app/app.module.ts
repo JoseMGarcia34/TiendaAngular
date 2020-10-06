eca4090b6766c866b2b34acb5d6af1ba4454ae8c
JmGarcia <josemanuelgarciamunoz@gmail.com>
2020-10-06 13:32:55 +0200

    8.4 doCheck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatefulComponent } from './stateful/stateful.component';
+import { StatelessComponent } from './stateless/stateless.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { StatusCartComponent } from './status-cart/status-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatefulComponent,
+    StatelessComponent,
+    ConfirmComponent,
+    StatusCartComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
